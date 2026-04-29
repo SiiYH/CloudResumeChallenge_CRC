459e0cc86bea42cd12fa3cb6f6f208a65b67af59
SiiYH <syewhei@gmail.com>
2026-04-30 07:05:20 +0800

    add visitor entity

--- backend/VisitorCounter.cs
+++ backend/VisitorCounter.cs
@@ -1,3 +1,14 @@
 using Azure;
 using Azure.Data.Tables;
+
 namespace CloudResume.Backend;
+
+public class VisitorEntity : ITableEntity
+{
+    public string PartitionKey { get; set; } = "resume";
+    public string RowKey { get; set; } = "visitorCount";
+    public int count { get; set; } = 0;
+    public ETag ETag { get; set; } = ETag.All;
+    public DateTimeOffset? Timestamp { get; set; }
+}
+
