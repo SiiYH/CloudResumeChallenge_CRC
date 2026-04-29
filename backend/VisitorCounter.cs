9897143d29d25e26c71b766c48d195eb3ccc493a
SiiYH <syewhei@gmail.com>
2026-04-30 07:13:37 +0800

    add credential to azure table

--- backend/VisitorCounter.cs
+++ backend/VisitorCounter.cs
@@ -1,5 +1,9 @@
 using Azure;
 using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace CloudResume.Backend;
 
@@ -12,3 +16,31 @@ public class VisitorEntity : ITableEntity
     public DateTimeOffset? Timestamp { get; set; }
 }
 
+public class VisitorCounter
+{
+    private readonly ILogger<VisitorCounter> _logger;
+
+    public VisitorCounter(ILogger<VisitorCounter> logger)
+    {
+        logger = _logger;
+    }
+
+    [Function("visitorCounter")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req
+    )
+    {
+        _logger.LogInformation("Visitor Counter triggered.");
+
+        var accountName = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_NAME");
+        var accountKey = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_KEY");
+        var tableName = Environment.GetEnvironmentVariable("TABLE_NAME");
+
+        var credential = new TableSharedKeyCredential(accountName, accountKey);
+        var client = new TableClient(
+            new Uri($"https://{accountName}.table.core.window.net"),
+            tableName,
+            credential
+        );
+    }
+}
